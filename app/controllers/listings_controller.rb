cf560c34ebf6e0725410ed960c6d220589682f06
Chris Lin <chrislin949@gmail.com>
2019-11-02 16:52:57 -0700

    add my_listings page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -61,6 +61,10 @@ class ListingsController < ApplicationController
     end
   end
 
+  def my_listings
+    @my_listings = current_user.listings
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_listing
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get  'static_pages/home'
   get  'static_pages/help'
   get '/signup', to: 'users#new'
+  get '/my_listings', to: 'listings#my_listings'
 end
