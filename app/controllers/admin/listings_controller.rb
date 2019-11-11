5ba0b99476b0434dc43b2d6b022595ab6da8f305
Vincent Cheung <vfhcheung@gmail.com>
2019-11-10 22:10:15 -0800

    Finish admin portal

--- app/controllers/admin/listings_controller.rb
+++ app/controllers/admin/listings_controller.rb
@@ -1,7 +1,42 @@
 class Admin::ListingsController < Admin::BaseController
+  before_action :set_listing, only: [:show, :edit, :update, :destroy]
 
   def index
     @listings = Listing.all
   end
 
+  def edit
+    @read_only_fields = Set[:id]
+  end
+
+  def update
+    respond_to do |format|
+      if @listing.update(listing_params)
+        format.html { redirect_to admin_listing_path(@listing), notice: 'Listing was successfully updated.' }
+        format.json { render :show, status: :ok, location: @listing }
+      else
+        format.html { render :edit }
+        format.json { render json: @listing.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @listing.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_listings_url, notice: 'Listing was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_listing
+      @listing = Listing.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def listing_params
+      schema = Listing.column_names.map &:to_sym
+      params.require(:listing).permit(schema)
+    end
 end
--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -1,7 +1,42 @@
 class Admin::ReservationsController < Admin::BaseController
+  before_action :set_reservation, only: [:show, :edit, :update, :destroy]
 
   def index
     @reservations = Reservation.all
   end
 
+  def edit
+    @read_only_fields = Set[:id]
+  end
+
+  def update
+    respond_to do |format|
+      if @reservation.update(reservation_params)
+        format.html { redirect_to admin_reservation_path(@reservation), notice: 'Reservation was successfully updated.' }
+        format.json { render :show, status: :ok, location: @reservation }
+      else
+        format.html { render :edit }
+        format.json { render json: @reservation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @reservation.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_reservations_url, notice: 'Reservation was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_reservation
+      @reservation = Reservation.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def reservation_params
+      schema = Reservation.column_names.map &:to_sym
+      params.require(:reservation).permit(schema)
+    end
 end
