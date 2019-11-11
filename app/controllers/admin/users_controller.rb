1bc696373e3e5559bde97589716739b8b2bb1b40
Vincent Cheung <vfhcheung@gmail.com>
2019-11-10 20:15:50 -0800

    Fix admin editing user bug

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,12 @@
 class Admin::UsersController < Admin::BaseController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :set_indexes, only: [:edit, :update]
 
   def index
     @users = User.all
+  end
+
+  def edit
+    @read_only_fields = Set[:id, :email, :reset_password_token, :confirmation_token, :unlock_token]
   end
 
   def update
@@ -23,14 +26,9 @@ class Admin::UsersController < Admin::BaseController
       @user = User.find(params[:id])
     end
 
-    def set_indexes
-      @indexes = Set[:email, :reset_password_token, :confirmation_token, :unlock_token]
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      schema = (User.column_names.map &:to_sym).to_set
-      permitted_params = schema - @indexes
-      params.require(:user).permit(permitted_params)
+      schema = User.column_names.map &:to_sym
+      params.require(:user).permit(schema)
     end
 end
