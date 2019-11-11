398c57a739a2b5cbc080a4c7c183654171cb1324
Vincent Cheung <vfhcheung@gmail.com>
2019-11-10 22:10:15 -0800

    Finish admin portal

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -21,6 +21,14 @@ class Admin::UsersController < Admin::BaseController
     end
   end
 
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
     def set_user
       @user = User.find(params[:id])
