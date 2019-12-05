2e2ca161831c0ff5c03b1b23d24834a6b4a5a065
leo-liu1 <leoliu1@g.ucla.edu>
2019-12-04 17:29:13 -0800

    Revert "Added basic fragment caching - TODO: implement russian doll caching"
    This reverts commit 68c32a8e6aa238f3b6ad7ee5582628b39865ed8c.

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,13 +1,2 @@
 module ListingsHelper
-  def cache_key_for_listing_row(listing)
-    "listing-#{listing.id}-#{listing.updated_at}"
-  end
-
-  def cache_key_for_listing_image_row(image)
-  	"listing-image-#{image.id}"
-  end
-
-  def cache_key_for_listing_review_row(review)
-  	"listing-review-#{review.id}-#{review.updated_at}"
-  end
 end
--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,5 +1,2 @@
 module ReservationsHelper
-  def cache_key_for_reservation_row(reservation)
-    "reservation-#{reservation.id}-#{reservation.updated_at}"
-  end
 end
