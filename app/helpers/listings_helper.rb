e6802e155590003b65354bd7e585296c2c820f22
leo-liu1 <leoliu1@g.ucla.edu>
2019-12-04 19:06:04 -0800

    Added russian doll caching and removed image caching

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,13 +1,30 @@
 module ListingsHelper
   def cache_key_for_listing_row(listing)
-    "listing-#{listing.id}-#{listing.updated_at}"
-  end
-
-  def cache_key_for_listing_image_row(image)
-  	"listing-image-#{image.id}"
+    "listing-#{listing.id}-#{listing.updated_at}-#{listing.images.count}"
   end
 
   def cache_key_for_listing_review_row(review)
   	"listing-review-#{review.id}-#{review.updated_at}"
+  end
+
+  def cache_key_for_listing_table_browse
+  	"listing-table-#{Listing.maximum(:updated_at)}"
+  end
+
+  def cache_key_for_listing_table_reserved
+  	max_reserved_updated_at = @reserved_listings.max_by(&:updated_at).updated_at
+  	"listing-table-#{max_reserved_updated_at}"
+  end
+
+  def cache_key_for_listing_table_available
+  	max_available_updated_at = @available_listings.max_by(&:updated_at).updated_at
+  	"listing-table-#{max_available_updated_at}"
+  end
+
+  def cache_key_for_listing_review_table
+  	if @reviews.length > 0
+  		max_review_updated_at = @reviews.max_by(&:updated_at).updated_at
+  	end
+  	"listing-review-table-#{@listing.id}-#{@listing.updated_at}-#{@listing.images.count}-#{max_review_updated_at}"
   end
 end
--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -2,4 +2,11 @@ module ReservationsHelper
   def cache_key_for_reservation_row(reservation)
     "reservation-#{reservation.id}-#{reservation.updated_at}"
   end
+
+  def cache_key_for_reservation_table
+  	if @reservations.length > 0
+  		max_reservation_updated_at = @reservations.max_by(&:updated_at).updated_at
+  	end
+  	"listing-table-#{max_reservation_updated_at}"
+  end
 end
